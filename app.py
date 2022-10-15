fdbedf5bd4f53b3e8bd6ec7dad2a5efd15d726bf
Sreejith Vidyadharan <sreejithvs1167@gmail.com>
2022-10-15 22:36:41 +0100

    Added zero-shot-classifier.py, requirements_file

--- app.py
+++ app.py
@@ -133,7 +133,7 @@ st.write('You selected:', options)
 
 
 
-# DAY 11 - Check Box
+# DAY 12 - Check Box
 
 st.header('st.checkbox')
 
@@ -182,7 +182,6 @@ st.latex(r'''
 
 
 
-
 # Day 16 - Customizing the theme of Streamlit apps
 
 
@@ -190,14 +189,14 @@ st.title('Customizing the theme of Streamlit apps')
 
 st.write('Contents of the `.streamlit/config.toml` file of this app')
 
-st.code("""
-[theme]
-primaryColor="#F39C12"
-backgroundColor="#2E86C1"
-secondaryBackgroundColor="#AED6F1"
-textColor="#FFFFFF"
-font="monospace"
-""")
+# st.code("""
+# [theme]
+# primaryColor="#F39C12"
+# backgroundColor="#2E86C1"
+# secondaryBackgroundColor="#AED6F1"
+# textColor="#FFFFFF"
+# font="monospace"
+# """)
 
 
 # SIDEBAR SLIDER 
@@ -243,6 +242,14 @@ if uploaded_file is not None:
   st.write(df.describe())
 else:
   st.info('☝️ Upload a CSV file')
+
+# test with below code as well
+w = st.file_uploader("Upload a CSV file", type="csv")
+if w:
+    import pandas as pd
+
+    data = pd.read_csv(w)
+    st.write(data)
 
 
 
@@ -312,7 +319,7 @@ st.balloons()
 
 
 
-# DAY - FORMS
+# DAY 22 - FORMS
 
 # st.form creates a form that batches elements together with a "Submit" button.
 
@@ -365,42 +372,370 @@ st.write('Selected value: ', selected_val)
 
 
 
+# DAY - 23 Retrieve URL Query Parameters
+
+# st.experimental_get_query_params allows the retrieval of query parameters directly from the URL of the user's browser.
+
+
+st.title('st.experimental_get_query_params')
+
+with st.expander('About this app'):
+     st.write("`st.experimental_get_query_params` allows the retrieval of query parameters directly from the URL of the user's browser.")
+
+
+# 1. Instructions
+st.header('1. Instructions')
+st.markdown('''In the above URL bar of your internet browser, append the following:`?firstname=Jack&surname=Beanstalk`''')
+
+# 2. Contents of st.experimental_get_query_params
+st.header('2. Contents of st.experimental_get_query_params')
+st.write(st.experimental_get_query_params())
+
+
+# 3. Retrieving and displaying information from the URL
+st.header('3. Retrieving and displaying information from the URL')
+
+firstname = st.experimental_get_query_params()['firstname'][0]
+surname = st.experimental_get_query_params()['surname'][0]
+
+
+st.write(f'Hello **{firstname} {surname}**, how are you?')
 
 
 
 
+# DAY 24 - CACHE
+
+# st.cache allows you to optimize the performance of your Streamlit app.
+
+# Streamlit provides a caching mechanism that allows your app to stay performant even when loading data from the web, manipulating large datasets, or performing expensive computations. This is done with the @st.cache decorator.
+
+# When you mark a function with the @st.cache decorator, it tells Streamlit that whenever the function is called it needs to check a few things:
+
+     # The input parameters that you called the function with
+     # The value of any external variable used in the function
+     # The body of the function
+     # The body of any function used inside the cached function
+
+# If all the components are the same it will run only once and cache it. The way Streamlit keeps track of changes in these components is through hashing. 
+
+
+from time import time
+
+st.title('st.cache')
+
+# Using cache
+a0 = time()
+st.subheader('Using st.cache')
+
+@st.cache(suppress_st_warning=True)
+def load_data_a():
+  df = pd.DataFrame(
+    np.random.rand(2000000, 5),
+    columns=['a', 'b', 'c', 'd', 'e']
+  )
+  return df
+
+st.write(load_data_a())
+a1 = time()
+st.info(a1-a0)
+
+
+# Not using cache
+b0 = time()
+st.subheader('Not using st.cache')
+
+def load_data_b():
+  df = pd.DataFrame(
+    np.random.rand(2000000, 5),
+    columns=['a', 'b', 'c', 'd', 'e']
+  )
+  return df
+
+st.write(load_data_b())
+b1 = time()
+st.info(b1-b0)
+
+
+# Notice how the first run may provide roughly similar run time. Reload the app and notice how the run time changes when using the st.cache decorator.
+
+
+
+# DAY 25 - Session State
+
+# st.session_state
+# We define access to a Streamlit app in a browser tab as a session. 
+# For each browser tab that connects to the Streamlit server, a new session is created. 
+# Streamlit reruns your script from top to bottom every time you interact with your app. 
+# Each reruns takes place in a blank slate: no variables are shared between runs.
+
+# Session State is a way to share variables between reruns, for each user session. 
+# In addition to the ability to store and persist state, Streamlit also exposes the ability to manipulate state using Callbacks.
+
+
+
+st.title('st.session_state')
+
+def lbs_to_kg():
+  st.session_state.kg = st.session_state.lbs/2.2046
+def kg_to_lbs():
+  st.session_state.lbs = st.session_state.kg*2.2046
+
+st.header('Input')
+col1, spacer, col2 = st.columns([2,1,2])
+with col1:
+  pounds = st.number_input("Pounds:", key = "lbs", on_change = lbs_to_kg)
+with col2:
+  kilogram = st.number_input("Kilograms:", key = "kg", on_change = kg_to_lbs)
+
+st.header('Output')
+st.write("st.session_state object:", st.session_state)
+
+# The above 2 custom functions will be called upon as soon as a numerical value is entered into the number box 
+# created using the st.number_input command. Notice how the on_change option specifies the 2 custom functions lbs_to_kg and kg_to_lbs).
+
+# In a nutshell, upon entering a number into the st.number_input box the number is converted by these custom functions.
+
+# Finally, the weight values in kg and lbs units is stored in the session state as 
+# st.session_state.kg and st.session_state.lbs will be printed out via st.write:
+
+
+
+# DAY 26 - How to use API -> BUILD A BORED API APP
+
+
+import requests
+
+st.title('🏀 Bored API app')
+
+st.sidebar.header('Input')
+selected_type = st.sidebar.selectbox('Select an activity type', ["education", "recreational", "social", "diy", "charity", "cooking", "relaxation", "music", "busywork"])
+
+suggested_activity_url = f'http://www.boredapi.com/api/activity?type={selected_type}'
+json_data = requests.get(suggested_activity_url)
+suggested_activity = json_data.json()
+
+c1, c2 = st.columns(2)
+with c1:
+  with st.expander('About this app'):
+    st.write('Are you bored? The **Bored API app** provides suggestions on activities that you can do when you are bored. This app is powered by the Bored API.')
+with c2:
+  with st.expander('JSON data'):
+    st.write(suggested_activity)
+
+st.header('Suggested activity')
+st.info(suggested_activity['activity'])
+
+col1, col2, col3 = st.columns(3)
+with col1:
+  st.metric(label='Number of Participants', value=suggested_activity['participants'], delta='')
+with col2:
+  st.metric(label='Type of Activity', value=suggested_activity['type'].capitalize(), delta='')
+with col3:
+  st.metric(label='Price', value=suggested_activity['price'], delta='')
 
 
 
 
+# DAY 27 - Build a draggable and resizable DASHBOARD with Streamlit Elements
 
 
+# First, we will need the following imports for our application.
 
+import json
+import streamlit as st
+from pathlib import Path
 
+# As for Streamlit Elements, we will need all these objects.
 
+from streamlit_elements import elements, dashboard, mui, editor, media, lazy, sync, nivo
 
+# Change page layout to make the dashboard take the whole page.
 
+# st.set_page_config(layout="wide")
 
+with st.sidebar:
+    st.title("🗓️ #30DaysOfStreamlit")
+    st.header("Day 27 - Streamlit Elements")
+    st.write("Build a draggable and resizable dashboard with Streamlit Elements.")
+    st.write("---")
 
+    # Define URL for media player.
+    media_url = st.text_input("Media URL", value="https://www.youtube.com/watch?v=vIQQR_yq-8I")
 
+# Initialize default data for code editor and chart.
 
+if "data" not in st.session_state:
+    st.session_state.data = Path("data.json").read_text()
 
+# Define a default dashboard layout.
+# Dashboard grid has 12 columns by default.
 
+layout = [
+    # Editor item is positioned in coordinates x=0 and y=0, and takes 6/12 columns and has a height of 3.
+    dashboard.Item("editor", 0, 0, 6, 3),
+    # Chart item is positioned in coordinates x=6 and y=0, and takes 6/12 columns and has a height of 3.
+    dashboard.Item("chart", 6, 0, 6, 3),
+    # Media item is positioned in coordinates x=0 and y=3, and takes 6/12 columns and has a height of 4.
+    dashboard.Item("media", 0, 2, 12, 4),
+]
 
+# Create a frame to display elements.
 
+with elements("demo"):
 
+    # Create a new dashboard with the layout specified above.
+    #
+    # draggableHandle is a CSS query selector to define the draggable part of each dashboard item.
+    # Here, elements with a 'draggable' class name will be draggable.
 
+    with dashboard.Grid(layout, draggableHandle=".draggable"):
 
+        # First card, the code editor.
+        #
+        # We use the 'key' parameter to identify the correct dashboard item.
+        #
+        # To make card's content automatically fill the height available, we will use CSS flexbox.
+        # sx is a parameter available with every Material UI widget to define CSS attributes.
 
+        with mui.Card(key="editor", sx={"display": "flex", "flexDirection": "column"}):
 
+            # To make this header draggable, we just need to set its classname to 'draggable',
+            # as defined above in dashboard.Grid's draggableHandle.
 
+            mui.CardHeader(title="Editor", className="draggable")
 
+            # We want to make card's content take all the height available by setting flex CSS value to 1.
+            # We also want card's content to shrink when the card is shrinked by setting minHeight to 0.
 
+            with mui.CardContent(sx={"flex": 1, "minHeight": 0}):
 
+                # Here is our Monaco code editor.
+                #
+                # First, we set the default value to st.session_state.data that we initialized above.
+                # Second, we define the language to use, JSON here.
+                #
+                # Then, we want to retrieve changes made to editor's content.
+                # By checking Monaco documentation, there is an onChange property that takes a function.
+                # This function is called everytime a change is made, and the updated content value is passed in
+                # the first parameter
+                #
+                # Streamlit Elements provide a special sync() function. This function creates a callback that will
+                # automatically forward its parameters to Streamlit's session state items.
+                #
+                # Examples
+                # --------
+                # Create a callback that forwards its first parameter to a session state item called "data":
+                # >>> editor.Monaco(onChange=sync("data"))
+                # >>> print(st.session_state.data)
+                #
+                # Create a callback that forwards its second parameter to a session state item called "ev":
+                # >>> editor.Monaco(onChange=sync(None, "ev"))
+                # >>> print(st.session_state.ev)
+                #
+                # Create a callback that forwards both of its parameters to session state:
+                # >>> editor.Monaco(onChange=sync("data", "ev"))
+                # >>> print(st.session_state.data)
+                # >>> print(st.session_state.ev)
+                #
+                # Now, there is an issue: onChange is called everytime a change is made, which means everytime
+                # you type a single character, your entire Streamlit app will rerun.
+                #
+                # To avoid this issue, you can tell Streamlit Elements to wait for another event to occur
+                # (like a button click) to send the updated data, by wrapping your callback with lazy().
 
+                editor.Monaco(
+                    defaultValue=st.session_state.data,
+                    language="json",
+                    onChange=lazy(sync("data"))
+                )
 
+            with mui.CardActions:
 
+                # Monaco editor has a lazy callback bound to onChange, which means that even if you change
+                # Monaco's content, Streamlit won't be notified directly, thus won't reload everytime.
+                # So we need another non-lazy event to trigger an update.
+                #
+                # The solution is to create a button that fires a callback on click.
+                # Our callback doesn't need to do anything in particular. You can either create an empty
+                # Python function, or use sync() with no argument.
+                #
+                # Now, everytime you will click that button, onClick callback will be fired, but every other
+                # lazy callbacks that changed in the meantime will also be called.
 
+                mui.Button("Apply changes", onClick=sync())
+
+        # Second card, the Nivo Bump chart.
+        # We will use the same flexbox configuration as the first card to auto adjust the content height.
+
+        with mui.Card(key="chart", sx={"display": "flex", "flexDirection": "column"}):
+
+            # To make this header draggable, we just need to set its classname to 'draggable',
+            # as defined above in dashboard.Grid's draggableHandle.
+
+            mui.CardHeader(title="Chart", className="draggable")
+
+            # Like above, we want to make our content grow and shrink as the user resizes the card,
+            # by setting flex to 1 and minHeight to 0.
+
+            with mui.CardContent(sx={"flex": 1, "minHeight": 0}):
+
+                # This is where we will draw our Bump chart.
+                #
+                # For this exercise, we can just adapt Nivo's example and make it work with Streamlit Elements.
+                #
+                # Data takes a dictionary as parameter, so we need to convert our JSON data from a string to
+                # a Python dictionary first, with `json.loads()`.
+
+                nivo.Bump(
+                    data=json.loads(st.session_state.data),
+                    colors={ "scheme": "spectral" },
+                    lineWidth=3,
+                    activeLineWidth=6,
+                    inactiveLineWidth=3,
+                    inactiveOpacity=0.15,
+                    pointSize=10,
+                    activePointSize=16,
+                    inactivePointSize=0,
+                    pointColor={ "theme": "background" },
+                    pointBorderWidth=3,
+                    activePointBorderWidth=3,
+                    pointBorderColor={ "from": "serie.color" },
+                    axisTop={
+                        "tickSize": 5,
+                        "tickPadding": 5,
+                        "tickRotation": 0,
+                        "legend": "",
+                        "legendPosition": "middle",
+                        "legendOffset": -36
+                    },
+                    axisBottom={
+                        "tickSize": 5,
+                        "tickPadding": 5,
+                        "tickRotation": 0,
+                        "legend": "",
+                        "legendPosition": "middle",
+                        "legendOffset": 32
+                    },
+                    axisLeft={
+                        "tickSize": 5,
+                        "tickPadding": 5,
+                        "tickRotation": 0,
+                        "legend": "ranking",
+                        "legendPosition": "middle",
+                        "legendOffset": -40
+                    },
+                    margin={ "top": 40, "right": 100, "bottom": 40, "left": 60 },
+                    axisRight=None,
+                )
+
+        # Third element of the dashboard, the Media player.
+
+        with mui.Card(key="media", sx={"display": "flex", "flexDirection": "column"}):
+            mui.CardHeader(title="Media Player", className="draggable")
+            with mui.CardContent(sx={"flex": 1, "minHeight": 0}):
+
+                media.Player(url=media_url, width="100%", height="100%", controls=True)
 
 
 
